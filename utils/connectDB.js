90fa73d6b49b09cf05610d909a9c742d60ddddc0
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-09-19 14:42:25 -0400

    enable logging

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -5,7 +5,7 @@ dotenv.config()
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  logging: false,
+  logging: true,
   pool: {
     min: 10,
     max: 50
